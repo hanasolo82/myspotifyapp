70017dea8b62b1238858b909ace3b903aa4b26b7
Hanasolo <hanasolo@hotmail.com>
2024-07-13 09:20:43 +0200

    searchbar finised

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,7 +5,11 @@ function SearchBar() {
   return (
     <div className={styles.Bar}>
       <label for="music-search"></label>
-      <input type="search" id="music-search" name="founder" placeholder="Search"></input>
+      <input 
+      className={styles.searchInput}
+      type="search"
+      id="music-search" name="founder" placeholder="Search">
+      </input>
     </div>
   );
 };
